db5236072f80c3c771f703ebfaf615583b3124ac
Suraj Nair <surajnair9698@gmail.com>
2026-02-25 18:57:36 +0530

    refactor: split server and routing logic into separate modules

---
+++ node-module-system/routes.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+function requestHandler(req, res) {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>This is my form page</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button>Submit</button></form></body>",
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>This is my page</title></head>");
+  res.write("<body><h1>hello rinzler</h1></body>");
+  res.write("</html>");
+  res.end();
+}
+
+module.exports.handler = requestHandler;
