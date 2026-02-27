46f2c8a11bd013e52b8ff04928eaef87f6649a2d
Suraj Nair <surajnair9698@gmail.com>
2026-02-27 23:08:09 +0530

    feat: handling different routes using Express Router

---
+++ Chapter2-Express/express-router/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.listen(3000);
