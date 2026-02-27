e882e51b150f9eb06717b23b98f57aef70a062b0
Suraj Nair <surajnair9698@gmail.com>
2026-02-27 13:57:41 +0530

    feat: create Express server with base middleware setup

--- Chapter2-Express/express init/app.js
+++ Chapter2-Express/express init/app.js
@@ -1,9 +1,16 @@
-const https = require("http");
-
+const http = require("http");
 const express = require("express");
 
 const app = express();
 
-const server = https.createServer(app);
+app.use((req, res, next) => {
+  console.log("inside the server");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send("<h1>Hello from express</h1>");
+});
+const server = http.createServer(app);
 
 server.listen(3000);
