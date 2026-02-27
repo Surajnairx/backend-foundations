76b9f82679e31996edf9910865e84ed30874c8e3
Suraj Nair <surajnair9698@gmail.com>
2026-02-27 14:23:02 +0530

    feat: add global middleware and /add-product route in Express server

--- Chapter2-Express/express init/app.js
+++ Chapter2-Express/express init/app.js
@@ -1,16 +1,20 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("inside the server");
+app.use("/", (req, res, next) => {
+  console.log("this always runs");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("inside the server");
+  res.send("<h1>Product Section</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("inside the server");
   res.send("<h1>Hello from express</h1>");
 });
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
