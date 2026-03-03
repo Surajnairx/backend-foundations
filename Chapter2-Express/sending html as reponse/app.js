92d5bcf3b48d29b31617e488e42b5d92b24865c9
Suraj Nair <surajnair9698@gmail.com>
2026-03-03 15:22:35 +0530

    feat: add error handling html page

--- Chapter2-Express/sending html as reponse/app.js
+++ Chapter2-Express/sending html as reponse/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shoupRoutes = require("./routes/shop");
+const path = require("path");
 const app = express();
 
 app.use(adminRoutes);
 app.use(shoupRoutes);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "error.html"));
+});
 
 app.listen(3000);
