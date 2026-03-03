548377dcdc0b06f872fb398da7d9351441278acd
Suraj Nair <surajnair9698@gmail.com>
2026-03-03 15:08:48 +0530

    chore: adding previous lesson files

---
+++ Chapter2-Express/prasing-incoming-requests/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Submit</button></form>",
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>hello from express!</h1>");
+});
+
+app.listen(3000);
