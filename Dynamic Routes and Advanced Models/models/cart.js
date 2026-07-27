a1307c3a15137970ca6c7c53f686179c79fa2179
Suraj Nair <surajnair9698@gmail.com>
2026-07-28 01:34:36 +0530

    feat:initialize Cart model and cart data path

---
+++ Dynamic Routes and Advanced Models/models/cart.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
+
+module.exports = class Cart {
+    static addProduct(id, productPrice) {      
+    //Fetch the previous cart
+    //Analyze the cart => Find existing product
+    // Add new product / increase quantity
+    }
+
+}
