10854545addfc3a7d170ca372eb5d641b4f39250
Suraj Nair <surajnair9698@gmail.com>
2026-09-02 19:06:12 +0530

    chore : add comments

--- Dynamic Routes and Advanced Models/app.js
+++ Dynamic Routes and Advanced Models/app.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs'); //for ejs template engine
+app.set('views', 'views');  //this is the default folder name for views, so this line is optional
+app.use(bodyParser.urlencoded({ extended: false }));    //this is a middleware that will parse the incoming request body and make it available in req.body
+app.use(express.static(path.join(__dirname, 'public'))); //this is a middleware that will serve static files from the public folder
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
+const adminRoutes = require('./routes/admin'); //this is a middleware that will handle all the routes that start with /admin
+const shopRoutes = require('./routes/shop'); //this is a middleware that will handle all the routes that start with /shop
+const errorController = require('./controllers/error'); //this is a middleware that will handle all the routes that are not handled by the above two middlewares
 
 
 app.use('/admin', adminRoutes);
